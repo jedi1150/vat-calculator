81b38d7e96156b2f2a3d10f7890bb2e224291b41
jedi1150 <6olegarh6@gmail.com>
2020-03-22 19:12:03 +0300

    New updates

---
+++ app/src/main/java/com/sandello/ndscalculator/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package com.sandello.ndscalculator
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
