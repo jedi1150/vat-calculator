1b407d9c98c2ea31da11a90e2e1156456141b3bb
jedi1150 <6olegarh6@gmail.com>
2020-04-22 22:53:20 +0300

    Retrieve rate values

--- app/src/main/java/com/sandello/ndscalculator/GetRates.kt
+++ app/src/main/java/com/sandello/ndscalculator/GetRates.kt
@@ -5,35 +5,33 @@ import android.os.StrictMode
 import androidx.room.Room
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.serialization.builtins.list
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
 import java.net.URL
 
+
 var listRates: List<Rate>? = null
+var url: String? = null
 
 class GetRates {
     fun main(context: Context) {
         val policy = StrictMode.ThreadPolicy.Builder()
                 .permitAll().build()
         StrictMode.setThreadPolicy(policy)
-        try {
-            val url = URL("http://jedioleg.asuscomm.com:8000").readText()
-            val json = Json(JsonConfiguration.Stable.copy(isLenient = true, ignoreUnknownKeys = true))
-            listRates = json.parse(Rate.serializer().list, url)
-            val db = Room.databaseBuilder(
-                    context,
-                    AppDatabase::class.java, "rates"
-            ).allowMainThreadQueries().build()
+        if (checkConnection(context)) {
             GlobalScope.launch(Dispatchers.IO) {
-                delay(1000L)
+                url = URL("http://jedioleg.asuscomm.com:8000").readText()
+                val json = Json(JsonConfiguration.Stable.copy(isLenient = true, ignoreUnknownKeys = true))
+                listRates = json.parse(Rate.serializer().list, url!!)
+                val db = Room.databaseBuilder(
+                        context,
+                        AppDatabase::class.java, "rates"
+                ).allowMainThreadQueries().build()
                 db.rateDao().deleteAll()
                 db.rateDao().insertAllCountries(listRates!!)
-
             }
-        } catch (e: Exception) {
         }
     }
 }
