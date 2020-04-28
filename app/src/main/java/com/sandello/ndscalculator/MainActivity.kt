abdcf44243d6342d2d4c51fac99b5af0ce4b5676
Яковлев Олег <6olegarh6@gmail.com>
2020-04-28 15:32:15 +0300

    Update

--- app/src/main/java/com/sandello/ndscalculator/MainActivity.kt
+++ app/src/main/java/com/sandello/ndscalculator/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         GetRates().main(this)
-
         setNightMode()
         setContentView(R.layout.activity_main)
     }
